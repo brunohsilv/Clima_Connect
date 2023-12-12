587cf47847e0c213474ad79254cafb8704db1ea5
GustavoDrews <ggustavostraliotto@gmail.com>
2023-12-11 23:36:43 -0300

    atualizações na largura do alerta

--- Clima_Connect/interface.py
+++ Clima_Connect/interface.py
@@ -3,23 +3,28 @@ import subprocess
 
 monitoramento_iniciado = False
 
-def iniciar_monitoramento(caminho_main, status_label, iniciar_button, finalizar_button):
+def iniciar_monitoramento(caminho_main, status_label, iniciar_button, finalizar_button, Alertas_text):
     global monitoramento_iniciado
     
-    status_label.config(text="Em monitoramento")
+    status_label.config(text="Em monitoramento...")
     iniciar_button.config(state=tk.DISABLED)
-    finalizar_button.config(state=tk.NORMAL, bg="red")  # Altera a cor para vermelho
+    finalizar_button.config(state=tk.NORMAL, bg="red")  
     monitoramento_iniciado = True
+
+    # Lê o conteúdo do arquivo PyWhatKit_DB.txt e exibe no campo de Alertas
+    with open("PyWhatKit_DB.txt", "r") as arquivo:
+        conteudo = arquivo.read()
+        Alertas_text.insert(tk.END, conteudo)
 
     # Inicia a execução do main.py
     subprocess.Popen(["python3", caminho_main])
 
-def finalizar_monitoramento(status_label, iniciar_button, finalizar_button):
+def finalizar_monitoramento(status_label, iniciar_button, finalizar_button, Alertas_text):
     global monitoramento_iniciado
     
-    status_label.config(text="Monitoramento finalizado")
+    status_label.config(text="Monitoramento finalizado.")
     iniciar_button.config(state=tk.NORMAL)
-    finalizar_button.config(state=tk.DISABLED, bg="gray")  # Volta para a cor cinza
+    finalizar_button.config(state=tk.DISABLED, bg="gray")
     monitoramento_iniciado = False
     
     # Encerra a execução do main.py
@@ -35,28 +40,28 @@ empresa_label.pack()
 # Carrega a imagem do logo
 logo_image = tk.PhotoImage(file="/home/gustavo/DocumentosProgramação/Clima_Connect/Clima_Connect/ClimaConnect-Logo.png")
 # Redimensiona a imagem do logo
-logo_image_resized = logo_image.subsample(5, 5)  # Substitua os números para redimensionar a imagem conforme necessário
+logo_image_resized = logo_image.subsample(5, 5)  
 logo_label = tk.Label(root, image=logo_image_resized)
-logo_label.image = logo_image_resized  # Mantém uma referência à imagem para que ela não seja descartada pelo garbage collector
+logo_label.image = logo_image_resized  
 logo_label.pack(side=tk.TOP, pady=10)
 
-status_label = tk.Label(root, text="Monitoramento não iniciado")
+status_label = tk.Label(root, text="Monitoramento não iniciado.")
 status_label.pack()
 
-iniciar_button = tk.Button(root, text="Iniciar Monitoramento", command=lambda: iniciar_monitoramento("/home/gustavo/DocumentosProgramação/Clima_Connect/Clima_Connect/main.py", status_label, iniciar_button, finalizar_button), bg="green")
+iniciar_button = tk.Button(root, text="Iniciar Monitoramento", command=lambda: iniciar_monitoramento("/home/gustavo/DocumentosProgramação/Clima_Connect/Clima_Connect/main.py", status_label, iniciar_button, finalizar_button, Alertas_text), bg="green")
 iniciar_button.pack()
 
-finalizar_button = tk.Button(root, text="Finalizar Monitoramento", command=lambda: finalizar_monitoramento(status_label, iniciar_button, finalizar_button), bg="gray", state=tk.DISABLED)
+finalizar_button = tk.Button(root, text="Finalizar Monitoramento", command=lambda: finalizar_monitoramento(status_label, iniciar_button, finalizar_button, Alertas_text), bg="gray", state=tk.DISABLED)
 finalizar_button.pack()
 
-terminal_frame = tk.Frame(root)
-terminal_frame.pack()
+Alertas_frame = tk.Frame(root)
+Alertas_frame.pack()
 
-terminal_label = tk.Label(terminal_frame, text="Terminal:")
-terminal_label.pack()
+Alertas_label = tk.Label(Alertas_frame, text="Alertas de monitoramento:")
+Alertas_label.pack()
 
-# Widget Text para exibir a saída do terminal
-terminal_text = tk.Text(terminal_frame, height=10, width=50)
-terminal_text.pack()
+# Widget Text para exibir a saída do Alertas
+Alertas_text = tk.Text(Alertas_frame, height=20, width=160)
+Alertas_text.pack()
 
 root.mainloop()
