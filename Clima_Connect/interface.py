3be9aef2b6e1632d621ab04c150da43746dbdfcd
GustavoDrews <ggustavostraliotto@gmail.com>
2023-12-11 22:29:23 -0300

    Interface alterada e funcionando

--- Clima_Connect/interface.py
+++ Clima_Connect/interface.py
@@ -1,21 +1,29 @@
 import tkinter as tk
-from connection import connect_to_rethinkdb, connect_to_postgresql
-import smtplib
-from email.mime.text import MIMEText
-import pywhatkit as kit
-import time
-from rethinkdb import RethinkDB
+import subprocess
 
-# Código para a interface gráfica usando Tkinter
-def iniciar_monitoramento():
-    # Aqui você pode integrar a lógica do seu código de monitoramento
-    # Coloque a lógica de monitoramento aqui
-    pass
+monitoramento_iniciado = False
 
-def finalizar_monitoramento():
-    # Aqui você pode adicionar a lógica para finalizar o monitoramento
-    # Isso pode incluir a interrupção dos loops de monitoramento ou outros processos
-    pass
+def iniciar_monitoramento(caminho_main, status_label, iniciar_button, finalizar_button):
+    global monitoramento_iniciado
+    
+    status_label.config(text="Em monitoramento")
+    iniciar_button.config(state=tk.DISABLED)
+    finalizar_button.config(state=tk.NORMAL, bg="red")  # Altera a cor para vermelho
+    monitoramento_iniciado = True
+
+    # Inicia a execução do main.py
+    subprocess.Popen(["python3", caminho_main])
+
+def finalizar_monitoramento(status_label, iniciar_button, finalizar_button):
+    global monitoramento_iniciado
+    
+    status_label.config(text="Monitoramento finalizado")
+    iniciar_button.config(state=tk.NORMAL)
+    finalizar_button.config(state=tk.DISABLED, bg="gray")  # Volta para a cor cinza
+    monitoramento_iniciado = False
+    
+    # Encerra a execução do main.py
+    subprocess.Popen(["pkill", "-f", "main.py"])
 
 root = tk.Tk()
 root.title("Monitoramento de Banco de Dados")
@@ -24,23 +32,31 @@ root.geometry("400x400")
 empresa_label = tk.Label(root, text="Clima Connect")
 empresa_label.pack()
 
-logo_image = tk.PhotoImage(file="/home/gustavo/DocumentosProgramação/Clima_Connect/ClimaConnect-Logo.png")
-logo_label = tk.Label(root, image=logo_image)
-logo_label.pack()
+# Carrega a imagem do logo
+logo_image = tk.PhotoImage(file="/home/gustavo/DocumentosProgramação/Clima_Connect/Clima_Connect/ClimaConnect-Logo.png")
+# Redimensiona a imagem do logo
+logo_image_resized = logo_image.subsample(5, 5)  # Substitua os números para redimensionar a imagem conforme necessário
+logo_label = tk.Label(root, image=logo_image_resized)
+logo_label.image = logo_image_resized  # Mantém uma referência à imagem para que ela não seja descartada pelo garbage collector
+logo_label.pack(side=tk.TOP, pady=10)
 
-status_label = tk.Label(root, text="Status: Em monitoramento")
+status_label = tk.Label(root, text="Monitoramento não iniciado")
 status_label.pack()
 
-# Adicionando um botão para iniciar o monitoramento
-iniciar_button = tk.Button(root, text="Iniciar Monitoramento", command=iniciar_monitoramento)
+iniciar_button = tk.Button(root, text="Iniciar Monitoramento", command=lambda: iniciar_monitoramento("/home/gustavo/DocumentosProgramação/Clima_Connect/Clima_Connect/main.py", status_label, iniciar_button, finalizar_button), bg="green")
 iniciar_button.pack()
 
-# Adicionando um botão para finalizar o monitoramento
-finalizar_button = tk.Button(root, text="Finalizar Monitoramento", command=finalizar_monitoramento)
+finalizar_button = tk.Button(root, text="Finalizar Monitoramento", command=lambda: finalizar_monitoramento(status_label, iniciar_button, finalizar_button), bg="gray", state=tk.DISABLED)
 finalizar_button.pack()
 
-# Terminal para exibir o histórico de prints
-terminal_text = tk.Text(root)
+terminal_frame = tk.Frame(root)
+terminal_frame.pack()
+
+terminal_label = tk.Label(terminal_frame, text="Terminal:")
+terminal_label.pack()
+
+# Widget Text para exibir a saída do terminal
+terminal_text = tk.Text(terminal_frame, height=10, width=50)
 terminal_text.pack()
 
 root.mainloop()
